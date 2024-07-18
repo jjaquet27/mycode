e83e728ce1576c060681fef74983c821cd31a1d7
Jeremy Jaquet <jeremy.jaquet@rotary.org>
2024-07-18 16:12:31 +0000

    terraform and Azure virtual networks

---
+++ azure-tf-tour/variables.tf
@@ -0,0 +1,15 @@
+# this is now an "input" value 
+variable "build_location" {
+  type    = string
+  default = "westus2"
+}
+
+# this is now an "input" value 
+variable "rg_name" {
+  type    = string
+  default = "myAzureRG"
+}
+
+variable "vnetname" {
+  default = "tflabvneta"
+}
